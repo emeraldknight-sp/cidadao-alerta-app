8ce881737d1265244914a3c8ee3cd192ef0bc0cf
David Almeida <contato.davidalmeida@outlook.com>
2024-09-25 10:45:39 -0300

    ✅ Add: Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import { Image, Text, View } from "react-native";
+import { Menu } from "lucide-react-native";
+import colors from "tailwindcss/colors"
+
+export default function Header() {
+  return (
+    <View className="flex-row justify-between items-center">
+      <View className="flex flex-row items-center gap-2">
+        <Image
+          source={require("@/src/assets/logo.png")}
+          className="w-[44px] h-[30px]"
+        />
+        <Text className="text-2xl font-semibold">Cidadão Alerta</Text>
+      </View>
+      <Menu size={32} color={colors.neutral[950]} />
+    </View>
+  );
+}
