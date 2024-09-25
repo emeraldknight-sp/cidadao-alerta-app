cb54a8f7e844c1c0d537c6faca9a6e3996e7db7a
David Almeida <contato.davidalmeida@outlook.com>
2024-09-25 16:26:58 -0300

    🏷️ Update types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,21 @@
+import { TouchableOpacityProps } from "react-native";
+
+declare module "accordion-collapse-react-native";
 declare module "*.png";
+
+interface ButtonProps extends TouchableOpacityProps {
+  children: React.ReactNode;
+}
+
+interface ButtonTextProps {
+  children: React.ReactNode;
+}
+
+interface ButtonIconProps {
+  children: React.ReactNode;
+}
+
+interface User {
+  email: string;
+  password: string;
+}
