ed57e42e5e62434568b7c76ba142429200961cc3
David Almeida <contato.davidalmeida@outlook.com>
2024-09-26 14:54:12 -0300

    ✅ Add: Home page

---
+++ src/app/home/_layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/src/components/header";
+import React from "react";
+import { Slot } from "expo-router";
+import { View } from "react-native";
+
+export default function HomeLayout() {
+  return (
+    <View className="bg-neutral-50 w-full py-10 px-4">
+      <Header />
+      <Slot />
+    </View>
+  );
+}
