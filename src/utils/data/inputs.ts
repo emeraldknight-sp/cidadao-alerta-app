a061690227a9325b8317a8248b377e4643d80238
David Almeida <contato.davidalmeida@outlook.com>
2024-09-27 11:26:37 -0300

    🤡 Mock things

---
+++ src/utils/data/inputs.ts
@@ -0,0 +1,32 @@
+export const INPUTS = [
+  {
+    id: 1,
+    name: "name",
+    text: "Nome:",
+    placeholder: "Informe seu nome",
+  },
+  {
+    id: 2,
+    name: "email",
+    text: "E-mail:",
+    placeholder: "Digite seu melhor e-mail",
+  },
+  {
+    id: 3,
+    name: "phone",
+    text: "Telefone:",
+    placeholder: "Digite seu número de telefone",
+  },
+  {
+    id: 4,
+    name: "defect",
+    text: "Categoria de defeito:",
+    placeholder: "",
+  },
+  {
+    id: 5,
+    name: "address",
+    text: "Endereço:",
+    placeholder: "Digite o endereço onde está o problema",
+  },
+];
