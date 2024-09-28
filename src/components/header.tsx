a7dd05c4633072e9ff08b3bfdd9c1e4f146512e4
David Almeida <contato.davidalmeida@outlook.com>
2024-09-28 15:58:12 -0300

    ✅ Update: Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import colors from "tailwindcss/colors"
 
 export default function Header() {
   return (
-    <View className="flex-row justify-between items-center">
+    <View className="bg-neutral-50 flex-row justify-between items-center z-10 py-2">
       <View className="flex flex-row items-center gap-2">
         <Image
           source={require("@/src/assets/logo.png")}
