1fd7124a2ab39df97abe08c674ecf0fd1a9ffe0b
David Almeida <contato.davidalmeida@outlook.com>
2024-09-29 11:48:02 -0300

    ✅ Update: Loading and Input components

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,6 +6,8 @@ export function Input({ ...rest }: TextInputProps) {
       textAlignVertical="center"
       placeholderTextColor="text-neutral-500"
       className="text-neutral-950 text-md font-body bg-neutral-50 rounded-md border-orange-500 border-2 h-12 px-4 py-2"
+      cursorColor="#f97316"
+      selectionColor="#fed7aa"
       {...rest}
     ></TextInput>
   );
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator, View } from "react-native";
 export function Loading() {
   return (
     <View className="flex-1 items-center justify-center bg-neutral-50">
-      <ActivityIndicator className="bg-neutral-50" />
+      <ActivityIndicator className="bg-neutral-50" color="#f97316" />
     </View>
   );
 }
