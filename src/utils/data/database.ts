5231dba6931900476200a756dc1d29977ebc3d9f
David Almeida <contato.davidalmeida@outlook.com>
2024-10-02 09:33:14 -0300

    ✅ Update: Database file

---
+++ src/utils/data/database.ts
@@ -0,0 +1,12 @@
+import { User } from "@/src/@types";
+
+export const DATABASE: User[] = [
+  {
+    name: "Lorena",
+    email: "lorena@gmail.com",
+    password: "12345678",
+    phone: "86988776655",
+    reports: [],
+    isLogged: false,
+  },
+];
