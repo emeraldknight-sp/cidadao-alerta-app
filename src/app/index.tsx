315847444f39b7dbc56109738bd17eae69304700
David Almeida <contato.davidalmeida@outlook.com>
2024-10-03 20:14:47 -0300

    ✅ Update: Main app file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,51 +1,8 @@
 import { Button } from "../components/button";
 import { Image, Text, View } from "react-native";
-import { Input } from "../components/input";
-import { User } from "../@types";
-import { toast } from "sonner-native";
-import { useRouter } from "expo-router";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  useForm,
-  Controller,
-  SubmitHandler,
-  FieldValues,
-} from "react-hook-form";
-import { DATABASE } from "../utils/data/database";
+import { router } from "expo-router";
 
-const schema = z.object({
-  email: z
-    .string({ required_error: "Campo obrigatório" })
-    .email("Seu e-mail deve ser válido"),
-  password: z
-    .string({ required_error: "Campo obrigatório" })
-    .min(8, "Sua senha deve ter 8 caracteres"),
-});
-
-export default function Login() {
-  const router = useRouter();
-
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: zodResolver(schema) });
-
-  const onSubmit: SubmitHandler<FieldValues> = ({ email, password }) => {
-    const user: User | undefined = DATABASE.find(
-      (userEl) => userEl.email === email
-    );
-
-    if (user && user?.password === password) {
-      router.push("/(app)/");
-    } else {
-      toast.error("Usuário não encontrado!", {
-        id: "user-not-found",
-      });
-    }
-  };
-
+export default function App() {
   return (
     <View className="flex flex-col justify-center items-center gap-6 flex-1 p-8">
       <View className="flex flex-col items-center gap-2">
@@ -57,56 +14,8 @@ export default function Login() {
           Cidadão Alerta
         </Text>
       </View>
-      <View className="w-full">
-        <Text className="text-sm text-neutral-950 font-heading">E-mail:</Text>
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input
-              inputMode="email"
-              autoComplete="email"
-              placeholder="Digite seu e-mail"
-              onChangeText={onChange}
-              onBlur={onBlur}
-              value={value}
-              blurOnSubmit={true}
-              returnKeyType="next"
-            />
-          )}
-        />
-        {errors.email && typeof errors.email.message === "string" && (
-          <Text className="text-red-500 text-xs font-bold">
-            {errors.email.message}
-          </Text>
-        )}
-      </View>
-      <View className="w-full">
-        <Text className="text-sm text-neutral-950 font-heading">Senha:</Text>
-        <Controller
-          control={control}
-          name="password"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input
-              autoComplete="password"
-              placeholder="Digite sua senha"
-              onChangeText={onChange}
-              onBlur={onBlur}
-              value={value}
-              secureTextEntry={true}
-              blurOnSubmit={true}
-              returnKeyType="next"
-            />
-          )}
-        />
-        {errors.password && typeof errors.password.message === "string" && (
-          <Text className="text-red-500 text-xs font-bold">
-            {errors.password.message}
-          </Text>
-        )}
-      </View>
-      <Button color="primary" onPress={handleSubmit(onSubmit)}>
-        <Button.Text>Entrar</Button.Text>
+      <Button color="primary" onPress={() => router.push("/(app)/")}>
+        <Button.Text>Começar</Button.Text>
       </Button>
     </View>
   );
